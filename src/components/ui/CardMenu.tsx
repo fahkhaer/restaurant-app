15349a833207348ed4bb1e82d43399c4087d345e
fahkhaer <khaeranifah@gmail.com>
2025-12-10 16:35:25 +0700

    refactor(cart-ui):add variant flex on Card Menu

--- src/components/ui/CardMenu.tsx
+++ src/components/ui/CardMenu.tsx
@@ -3,36 +3,72 @@ import { cn } from '@/lib/utils';
 
 type CardProps = {
   className?: string;
+  imgClassName?: string;
   name?: string;
   price?: string;
   image?: string | null;
   style?: React.CSSProperties;
   rightContent?: ReactNode;
+  variant?: 'default' | 'flex';
 };
 
 function CardMenu({
   className,
+  imgClassName,
   name,
   price,
   image,
   style,
   rightContent,
+  variant = 'default',
 }: CardProps) {
   return (
-    <div className={cn('rounded-2xl', className)} style={style}>
-      <img
-        className='size-[285px] rounded-tr-2xl rounded-tl-2xl'
-        src={image || './src/assets/images/price.png'}
-        alt='food-menu'
-      />
-      <div className='flex-between p-3 md:p-4 '>
-        <div>
-          <span className='text-md-medium'>{name}</span>
-          <h1 className='text-lg-extrabold'>Rp.{price}</h1>
+    <>
+      {variant === 'default' && (
+        <div className={cn('rounded-2xl', className)} style={style}>
+          <img
+            className='size-[285px] rounded-t-2xl'
+            src={image || './src/assets/images/price.png'}
+            alt='food-menu'
+          />
+          <div className='flex-between p-3 md:p-4'>
+            <div>
+              <span className='text-md-medium'>{name}</span>
+              <h1 className='text-lg-extrabold'>Rp.{price}</h1>
+            </div>
+            {rightContent}
+          </div>
         </div>
-        {rightContent}
-      </div>
-    </div>
+      )}
+      {variant === 'flex' && (
+        <div
+          className={cn(
+            'rounded-2xl flex justify-between items-center',
+            className
+          )}
+          style={style}
+        >
+          <div className='flex'>
+            <img
+              className={cn(
+                'size-[285px] rounded-l-2xl object-cover',
+                imgClassName
+              )}
+              src={image || './src/assets/images/price.png'}
+              alt='food-menu'
+            />
+
+            <div className='flex-between p-3 md:p-4'>
+              <div>
+                <span className='text-md-medium'>{name}</span>
+                <h1 className='text-lg-extrabold'>Rp.{price}</h1>
+              </div>
+            </div>
+          </div>
+          {rightContent}
+        </div>
+      )}
+    </>
   );
 }
 
