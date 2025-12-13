90fcb7b5fde6fe798ffec044e403d80a35fc39e5
fahkhaer <khaeranifah@gmail.com>
2025-12-13 10:54:13 +0700

    feat(detail-api): add types of resrtaurant.ts

---
+++ src/types/restaurant.ts
@@ -0,0 +1,24 @@
+export type Menu = SampleMenu;
+
+export type SampleMenu = {
+  id: number;
+  foodName: string;
+  price: number;
+  type: string;
+  image: string;
+};
+
+export type RecommendationItem = {
+  id: number;
+  name: string;
+  star: number;
+  place: string;
+  lat: number;
+  long: number;
+  logo: string;
+  images: string[];
+  category: string;
+  reviewCount: number;
+  sampleMenus: SampleMenu[];
+  isFrequentlyOrdered: boolean;
+};
