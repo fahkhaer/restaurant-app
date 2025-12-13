55b106d1ac514623d970a20b0c3269690b8f59d5
fahkhaer <khaeranifah@gmail.com>
2025-12-13 20:03:36 +0700

    feat(update profile-api)

--- src/services/api/profile.ts
+++ src/services/api/profile.ts
@@ -13,11 +13,36 @@ export function GetProfile() {
         },
       });
 
-      console.log('getpr', res.data.data);
-
       return res.data.data;
     },
     refetchOnWindowFocus: true,
     refetchOnReconnect: true,
   });
+}
+
+export async function UpdateProfile(data: {
+  name: string;
+  email: string;
+  phone: string;
+  avatar?: File;
+}) {
+  const token = localStorage.getItem('token');
+
+  const formData = new FormData();
+  formData.append('name', data.name);
+  formData.append('email', data.email);
+  formData.append('phone', data.phone);
+  if (data.avatar) {
+    formData.append('avatar', data.avatar);
+  }
+
+  const res = await axios.put(`${baseUrl}/api/auth/profile`, formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  console.log('post', res);
+
+  return res.data;
 }
