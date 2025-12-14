5e394c6b1d98c50d05aad1ee7f4746580d206bc4
fahkhaer <khaeranifah@gmail.com>
2025-12-14 16:39:29 +0700

    feat(cart-api): apply api to page Cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,16 +1,34 @@
-import MyOrderCard from '@/components/MyOrderCard';
+
+import CartCard from '@/components/CartCard';
 import { Button } from '@/components/ui/button';
+import { GetCart } from '@/services/api/cart';
 import Container from '@/styles/Container';
+import type { CartRestaurant } from '@/types/cart';
 
 function Cart() {
+  const { data, isLoading, isError } = GetCart();
+  console.log('pagecart', data);
+
+  if (isLoading) return <p>Loading profile...</p>;
+  if (isError) return <p>Error loading profile</p>;
   return (
     <Container className='mb-25'>
       <h1 className='pb-8'>My Cart</h1>
-      <MyOrderCard
-        rightContent={
-          <Button className='w-60 h-12 text-[#FDFDFD]'>Checkout</Button>
-        }
-      />
+      <div className='space-y-5'>
+        {data?.cart?.length > 0 ? (
+          data?.cart?.map((item: CartRestaurant) => (
+            <CartCard
+              key={item?.restaurant?.id}
+              order={item}
+              rightContent={
+                <Button className='w-60 h-12 text-[#FDFDFD]'>Checkout</Button>
+              }
+            />
+          ))
+        ) : (
+          <p>Your cart is empty.</p>
+        )}
+      </div>
     </Container>
   );
 }
