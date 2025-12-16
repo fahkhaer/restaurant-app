3e9dda0b1cbe088080d42392f59d5acf93adce0c
fahkhaer <khaeranifah@gmail.com>
2025-12-16 11:11:43 +0700

    feat(review-api): add types of api

--- src/types/reviews.ts
+++ src/types/reviews.ts
@@ -1,13 +1,46 @@
-interface User {
+export type Review = {
+  id: number;
+  star: number;
+  comment: string;
+  transactionId: string;
+  createdAt: string;
+  user: ReviewUser;
+  restaurant: ReviewRestaurant;
+  menus: ReviewMenu[];
+};
+
+
+export type CreateReviewPayload = {
+  transactionId: string;
+  restaurantId: number;
+  star: number; 
+  comment: string;
+  menuIds: number[];
+};
+
+export type ReviewUser = {
   id: number;
   name: string;
-  avatar: string;
-}
+};
 
-export interface Review {
-  id?: number;
-  star?: number;
-  comment?: string;
-  createdAt?: string;
-  user?: User;
-}
+export type ReviewRestaurant = {
+  id: number;
+  name: string;
+};
+
+export type ReviewMenu = {
+  menuId: number;
+  menuName: string;
+  price: number;
+  type: 'food' | 'drink';
+  image: string;
+  quantity: number;
+};
+
+export type CreateReviewResponse = {
+  success: boolean;
+  message: string;
+  data: {
+    review: Review;
+  };
+};
